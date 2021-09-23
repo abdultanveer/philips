2bca49a357e7269080f47bad5c7641023e993cca
abdul <abdul.tanveer@gmail.com>
2021-09-23 14:08:35 +0530

    getting data from edittext and setting on a textview

--- app/src/main/java/com/abdul/philips/MainActivity.kt
+++ app/src/main/java/com/abdul/philips/MainActivity.kt
@@ -4,22 +4,30 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var mButton:Button //declaration
+    lateinit var etName: EditText
+    lateinit var tvRes: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main) //inflating -- R.layout
 
-        mButton = findViewById(R.id.button)
-        //step1 -- user has to press the button
-        //step 2 -- after step 1 show the toast
-        mButton.setOnClickListener(View.OnClickListener {
-            Toast.makeText(this,"button was clicked",Toast.LENGTH_SHORT).show()
-        })
+        etName = findViewById(R.id.etName)  //what is findviewbyid
+        tvRes = findViewById(R.id.textView) //what is R.id
+    }
+
+    fun clickHandler(view: View) {
+        //get the data from edittext
+        //String name = etName.gettext().toString();
+        var name:String = etName.text.toString()
+        //put that data into the textview
+        //tvRes.setText(name);
+        tvRes.text = name
     }
 
 
